0e9bafa3a193115d02feaab80af673bb56e6510b
Dominik Z <domyz257@gmail.com>
2023-05-24 14:57:53 +0300

    dd

--- email.js
+++ email.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+import logo from "./logo.png";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -66,13 +67,6 @@ const sendCancellationEmail = async (email, reason) => {
   await transporter.sendMail(mailOptions);
 };
 const sendVerificationEmail = async (email, token) => {
-  const transporter2 = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: email,
@@ -83,16 +77,16 @@ const sendVerificationEmail = async (email, token) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>El. pašto patvirtinimasn</title>
     </head>
-    <body style="width:100%; height:100%; margin:0; padding:32px; font: normal normal normal 14px/21px Arial,sans-serif; color:#333; background-color:#f1f1f1; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;">
+    <body style="width:100%; height:100%; margin:0; padding:32px; font: normal normal normal 14px/21px Arial,sans-serif; color:#333;-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;">
         <table class="email-wrapper" style="width:100%; height:100%; margin:auto; padding:0; text-align:center; vertical-align:middle; border-spacing:0; border-collapse:collapse;"><tr><td>
         
         <table class="email-layout" style="width:450px; height:300px; margin:auto; padding:0; vertical-align:middle; border-spacing:0; border-collapse:collapse;">
             <thead class="email-header" style="text-align:center;"><tr><th style="padding-bottom:32px; text-align:center; font-weight:normal;">            
-                <a href="https://gerapagalba.lt" target="_blank" style="text-decoration:none; color:#446cb3 !important;"> 
+                <a href="https://gerapagalba.lt" target="_blank"><img style="width:180px; border:none;" src="${logo.src}"></img></a>
             </th></tr></thead>
             
             <tbody class="email-body"><tr><td style="text-align:left;">     
-                <div style="padding:21px 32px; background-color:#fff; border-bottom:2px solid #e1e1e1; border-radius:3px;">
+                <div style="padding:21px 32px; background-color: #f3f3f3; border-bottom:2px solid #e1e1e1; border-radius:3px;">
                     <h1 style="font-size:21px; line-height:30px; font-weight:bold;">El. pašto patvirtinimas</h1>
     
                     <p style="padding:11px 0; text-align:left;">
@@ -116,7 +110,7 @@ const sendVerificationEmail = async (email, token) => {
     </html>`,
   };
 
-  await transporter2.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 };
 module.exports = {
   sendPasswordResetEmail,
